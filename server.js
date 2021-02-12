dc4f9dafa095d3562451610121294abe15732431
Shivey Singh <shivey_khangura@hotmail.com>
2021-02-12 18:18:40 +0000

    sorted out bugs for heroku

--- server.js
+++ server.js
@@ -15,11 +15,8 @@ app.use(express.json());
 
 app.use(express.static("public"));
 
-mongoose.connect(process.env.MONGODB_URI || "mongodb://localhost/budget", {
-  useNewUrlParser: true,
-  useFindAndModify: false,
-  useUnifiedTopology: true
-})
+var MONGODB_URI = process.env.MONGODB_URI || "mongodb://localhost/budget";
+mongoose.connect(MONGODB_URI);
 
 // routes
 app.use(require("./routes/api.js"));
